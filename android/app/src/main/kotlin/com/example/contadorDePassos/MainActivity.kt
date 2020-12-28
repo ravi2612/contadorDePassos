64082f254ad835de5084f0a38cb11dbf1b903e96
ravi2612 <ravinavarro26@gmail.com>
2020-12-28 01:15:09 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/contadorDePassos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.contadorDePassos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Contador de Pessoas',
+    home: Container(
+      color: Colors.white,
+      ),
+    
+  ));
+}
