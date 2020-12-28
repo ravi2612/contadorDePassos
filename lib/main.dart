83a7544565baaf9b88a085a1f14090d124f17f07
ravi2612 <ravinavarro26@gmail.com>
2020-12-28 14:43:21 -0300

    background

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,66 @@
+import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     title: 'Contador de Pessoas',
-    home: Container(
-      color: Colors.white,
+    home: Stack(
+      children: [
+        Image.asset(
+          'assets/images/restaurant.jpg',
+          fit: BoxFit.cover,   
+          height: 1000,       
+        ),
+         Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Pessoas : 0',
+        style: TextStyle(color: 
+        Colors.white,
+        fontWeight:FontWeight.bold,
+        ),
       ),
-    
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+           Padding(
+             padding: const EdgeInsets.all(10.0),
+             child: FlatButton(
+               child: Text('+1',
+               style:TextStyle(
+                 fontSize: 40,
+                 color: Colors.white             
+               ),
+               ),
+               onPressed: () {},
+             ),
+           ), 
+           
+            Padding(
+              padding: const EdgeInsets.all(10.0),              
+              child: FlatButton(
+               child: Text('-1',
+               style:TextStyle(
+                 fontSize: 40,
+                 color: Colors.white             
+               ),
+               ),
+               onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      Text('Pode Entrar!!',
+      style: TextStyle(
+        color:Colors.white,
+        fontStyle: FontStyle.italic,
+        fontSize:30,
+          ),
+        ),
+      ],
+    ),
+      ],
+    ),
   ));
 }
